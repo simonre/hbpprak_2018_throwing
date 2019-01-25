5f26636d29e0e24995fce7c54487b2c9e674a23d
Simon Reinkemeier <simon.reinkemeier@student.kit.edu>
2019-01-25 20:05:49 +0100

    somewhat working example with genetic

--- get_obj_pos.py
+++ get_obj_pos.py
@@ -2,11 +2,15 @@
 import numpy as np
 import sensor_msgs.msg
 
-@nrp.MapCSVRecorder("obj_recorder", filename="obj_position.csv", headers=["Time", "px", "py", "pz"])
+@nrp.MapRobotPublisher("topic_index_distal", Topic('/robot/hand_j14/cmd_pos', std_msgs.msg.Float64))
+@nrp.MapCSVRecorder("obj_recorder", filename="obj_positions.csv", headers=["Time", "px", "py", "pz"])
 @nrp.Robot2Neuron()
-def get_pos(t, obj_recorder):
+def get_pos(t, topic_index_distal, obj_recorder):
     from rospy import ServiceProxy
     from gazebo_msgs.srv import GetModelState
+    clientLogger.info("distal pos = " + str(topic_index_distal))
+    print(str(topic_index_distal))
+
     model_name = 'cylinder'
     state_proxy = ServiceProxy('/gazebo/get_model_state', GetModelState, persistent=False)
     cylinder_state = state_proxy(model_name, "world")
--- get_pos.py
+++ get_pos.py
@@ -2,14 +2,24 @@
 import numpy as np
 import sensor_msgs.msg
 
-@nrp.MapCSVRecorder("obj_recorder", filename="obj_position.csv", headers=["Time", "px", "py", "pz"])
+
+@nrp.MapRobotSubscriber("topic_index_distal", Topic('/robot/hand_Index_Finger_Distal/cmd_pos', std_msgs.msg.Float64))
+@nrp.MapCSVRecorder("obj_recorder", filename="obj_position.csv", headers=["Time", "px", "py", "pz", "distal_pos"])
 @nrp.Robot2Neuron()
-def get_pos(t, obj_recorder):
+def get_pos(t, topic_index_distal, obj_recorder):
     from rospy import ServiceProxy
     from gazebo_msgs.srv import GetModelState
+
+    distal_pos_msg = topic_index_distal.value
+    distal_pos = 0
+    if 'data' in dir(distal_pos_msg):
+    	distal_pos = distal_pos_msg.data
+
     model_name = 'cylinder'
     state_proxy = ServiceProxy('/gazebo/get_model_state', GetModelState, persistent=False)
     cylinder_state = state_proxy(model_name, "world")
-    if cylinder_state.success:
+    if cylinder_state.success and distal_pos != 0:
         current_position = cylinder_state.pose.position
-        obj_recorder.record_entry(t, current_position.x, current_position.y, current_position.z)
+        clientLogger.info(current_position)
+        clientLogger.info(distal_pos)
+        obj_recorder.record_entry(t, current_position.x, current_position.y, current_position.z, distal_pos)
